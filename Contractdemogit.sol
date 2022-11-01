a73846fefa8a311f6bc2cefa643f7bca55d8125a
tuongziperno1 <111478954+tuongziperno1@users.noreply.github.com>
2022-11-01 14:20:49 +0700

    Create Contractdemogit.sol

---
+++ Contractdemogit.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.11;
+
+contract myContractgit {
+    uint storedData;
+
+    function set(uint x) public{
+        storedData = x;
+    }
+    function get() public view returns (uint){
+        return storedData
+    }
+}
